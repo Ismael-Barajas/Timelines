19ec662557c053966ae20c6dc34ee4319b409203
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-02-22 16:06:50 -0800

    got axios working

--- pages/testpage.js
+++ pages/testpage.js
@@ -22,4 +22,47 @@ export default function Test() {
             }
         </div>
     )
-}
+
+// const getrepos = async () => {
+//   const data = await fetchPublicRepos();
+//   //const aData = Object.values(data);
+//   return data;
+// };
+
+// export default function Test() {
+//   const data = getrepos();
+//   console.log(data);
+//   //return <pre>{JSON.stringify(data, null, 2)}</pre>;
+//   return (
+//     <div>
+//       {/* {data.map((repos) => (
+//         <p>{repos.full_name}</p>
+//       ))} */}
+//     </div>
+//   );
+// }
+
+// class TestR extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       data: [],
+//     };
+//   }
+
+//   async componentDidMount() {
+//     const data = await fetchPublicRepos();
+//     console.log(data);
+//     this.setState({ data });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <pre>{JSON.stringify(this.state.data, null, 2)}</pre>
+//       </div>
+//     );
+//   }
+// }
+
+// export default TestR;
