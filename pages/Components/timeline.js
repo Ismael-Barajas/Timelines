93b6823cb21f20e770cc5c935845211dba749a2d
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-03-02 17:29:14 -0800

    added states and set up

---
+++ pages/Components/timeline.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+//from my current understanding, data should be sent to the this timeline component
+//from the index page. after the search goes through
+export const Timeline = (dataProps) => {
+  const [timelineData, setTimelineData] = useState(0);
+  useEffect(() => {});
+
+  return (
+    <div>
+      timelinestuff
+      <div>memes</div>
+    </div>
+  );
+};
+//next I wanna set this up to be able to use my repo data while
+//so that when the search functionality is complete, this component should be ready
+//for its data.
+//We need to choose what css library we want to use.
