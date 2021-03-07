7f09ae45676febd5d1bb50ac9b9ea6c2f8bb7467
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-03-06 16:11:41 -0800

    testing

--- pages/index.js
+++ pages/index.js
@@ -6,47 +6,50 @@ const SearchPage = () => {
   // activities list to pass to timeline component
   const [activitiesList, setActivitiesList] = useState([]);
   // state for setting error messages returned - pass to error component
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   // Prop function passed to searchbar
-  const search = searchValue => {
+  const search = (searchValue) => {
     const url = `https://api.github.com/users/${searchValue}/repos`;
-    fetch(url).then(response => response.json()).then(json => {
-      if (json && json.length) {
-        console.log('got json response ', json);
-        setActivitiesList(json);
-      } else if (json && json.message) {
-        // receive back some json error message
-        setError(json.message);
-        console.log('No user found ');
-      }
-    })
-  }
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json && json.length) {
+          console.log("got json response ", json);
+          setActivitiesList(json);
+        } else if (json && json.message) {
+          // receive back some json error message
+          setError(json.message);
+          console.log("No user found ");
+        }
+      });
+  };
 
-  const ownerName = activitiesList && activitiesList.length &&
-  activitiesList[0].owner.login ?activitiesList[0].owner.login : "";
+  const ownerName =
+    activitiesList && activitiesList.length && activitiesList[0].owner.login
+      ? activitiesList[0].owner.login
+      : "";
 
   return (
+    <div>
+      <SearchBar search={search} />
       <div>
-        <SearchBar search={search} />
-        <div>
-          <h1 className={styles.ownerTitle}>{ownerName}</h1>
-          {activitiesList.map((item, index) => (
-              <div className={styles.items} key={index}>
-                <h2> {item.name} </h2>
-                {item.description && (
-                    <p>
-                      <b>Description: </b>
-                      {item.description}
-                    </p>
-                )}
-                <p> {item.updated_at} </p>
-              </div>
-          ))}
-        </div>
+        <h1 className={styles.ownerTitle}>{ownerName}</h1>
+        {activitiesList.map((item, index) => (
+          <div className={styles.items} key={index}>
+            <h2> {item.name} </h2>
+            {item.description && (
+              <p>
+                <b>Description: </b>
+                {item.description}
+              </p>
+            )}
+            <p> {item.updated_at} </p>
+          </div>
+        ))}
       </div>
-  )
-
-}
+    </div>
+  );
+};
 
 export default SearchPage;
