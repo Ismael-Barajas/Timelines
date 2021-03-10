9dfeb8995cbe0ce04fd947575526546fcb41d84f
diginomadik <55097568+diginomadik@users.noreply.github.com>
2021-03-10 10:28:08 -0800

    added header

--- pages/Components/searchbar.js
+++ pages/Components/searchbar.js
@@ -25,6 +25,8 @@ const SearchBar = (props) => {
 
     return (
         <form className={'search'}>
+            <h1 style={{ color : 'white', backgroundColor: '#4B4545', width: '1440px', height: '478px', left: '0px', right: '100px', top: '0px'}}>Github Timeline</h1>
+            <h2 style={{ color : 'black', backgroundColor: 'grey', width: '1440px', height: '278px', left: '0px', top: '0px'}}>SEARCH AWAY</h2>
             <input
                 placeholder={"Enter github username"}
                 style={BarStyles}
