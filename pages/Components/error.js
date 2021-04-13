fddcdd8683b54da4027f6a71f63cacfc1fa44506
Britney <britneyf@Britneys-MacBook-Pro.local>
2021-04-12 18:55:12 -0700

    error styling changes - karla & britney

--- pages/Components/error.js
+++ pages/Components/error.js
@@ -1,5 +1,6 @@
 //create a error component when user does not exist
 import React from 'react';
+import Image from 'next/image'
 
 
 class ErrorBoundary extends React.Component {
@@ -12,16 +13,19 @@ class ErrorBoundary extends React.Component {
         // You can also log the error to an error reporting service
         logErrorToMyService(error, errorInfo);
     }
-
     render() {
         const { errorMessage } = this.props;
         //if errorMessage received from parent then render
         return (
             <div>
-                <h1>{ !!errorMessage && errorMessage }</h1>
+                <style>{'body { background-color: #F0F0E2; }'}</style>
+                <h1><center>{ !!errorMessage && errorMessage }</center></h1>
+                <center><Image src="/githubLogo.png" alt="github logo" class="center" width="500" height="250"/></center>
+                <h2><center><font size="200">ERROR:</font></center></h2>
+                <p><center><font size = "5"><style>{'body { font-family : Playfair Display; }'}</style>USER DOES NOT EXIST</font></center></p>
             </div>
         );
     }
 }
 
-  export default ErrorBoundary;
+export default ErrorBoundary;
