6896603550d22aa2ce70c82883482e96b98fa8ca
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-05-04 20:53:20 -0700

    Now displays contributions for each repo and total contributions

--- Components/timeline.js
+++ Components/timeline.js
@@ -19,10 +19,11 @@ import {
   DeviceHub as DeviceHubIcon,
   Star as StarIcon,
 } from "@material-ui/icons";
-import { useState } from "react";
+import CountUp from "react-countup";
 
 const Timeline = (props) => {
   let timelineData = props.data;
+  let totalContributions = 0;
 
   if (timelineData && !timelineData.length) {
     return null;
@@ -39,56 +40,12 @@ const Timeline = (props) => {
     return Date.parse(b.updated_at) - Date.parse(a.updated_at);
   });
 
-  //  const requestContributors = async (url) => {
-  //    let contributions;
-  //    let response = await fetch(url).then((res) => res.json());
-  //    if (response && response.length) {
-  //      const owner = response.filter(
-  //        (contributor) => ownerName === contributor.login
-  //      );
-  //      if (owner && owner[0] && owner[0].contributions) {
-  //        contributions = owner[0].contributions;
-  //      }
-  //    }
-  //    return contributions;
-  //  };
+  timelineData.forEach((item) => {
+    totalContributions += item.contributions;
+  });
 
-  //  if (timelineData && !timelineData.length) {
-  //    return null;
-  //  } else {
-  //    timelineData = timelineData.map((item) => {
-  //      let itemData = {};
-  //      let contributions;
-  //      if (item && item.contributors_url) {
-  //        contributions = Promise.resolve(
-  //          requestContributors(item.contributors_url)
-  //        ).then((value) => {
-  //          //setIsLoading(false);
-  //        });
-  //      }
-  //      itemData = {
-  //        owner: {
-  //          login: item.owner.login,
-  //          html_url: item.owner.html_url,
-  //          avatar_url: item.owner.avatar_url,
-  //        },
-  //        updated_at: item.updated_at,
-  //        html_url: item.html_url,
-  //        name: item.name,
-  //        description: item.description,
-  //        homepage: item.homepage,
-  //        language: item.language,
-  //        watchers_count: item.watchers_count,
-  //        stargazers_count: item.stargazers_count,
-  //        forks: item.forks,
-  //        contributors_url: item.contributors_url,
-  //        contributions,
-  //      };
-  //      return itemData;
-  //    });
-  //  }
+  totalContributions = 1000;
 
-  console.log(timelineData);
   return (
     <Grid
       container
@@ -99,107 +56,132 @@ const Timeline = (props) => {
     >
       <Grid item xs={12}>
         <Paper component="form" className={styles.paper} elevation={8}>
-          <div className={styles.usernameDiv}>
-            <Image
-              src={ownerAvatar}
-              alt="avatar"
-              width="50"
-              height="50"
-              layout="intrinsic"
-            />
-            <h1 className={styles.h1tag}>
-              <a href={ownerURL} target="_blank">
-                {ownerName}
-              </a>
-            </h1>
+          <div className={styles.headDiv}>
+            <Card className={styles.headCard} variant="outlined">
+              <CardContent align="center">
+                <div className={styles.usernameDiv}>
+                  <Image
+                    src={ownerAvatar}
+                    alt="avatar"
+                    width="50"
+                    height="50"
+                    layout="intrinsic"
+                  />
+                  <h1 className={styles.h1tag}>
+                    <a href={ownerURL} target="_blank">
+                      {ownerName}
+                    </a>
+                  </h1>
+                </div>
+                <div
+                  className={
+                    totalContributions >= 1000
+                      ? styles.totalContribDiv1000
+                      : totalContributions >= 100 && totalContributions < 1000
+                      ? styles.totalContribDiv100
+                      : totalContributions >= 50 && totalContributions < 100
+                      ? styles.totalContribDiv50
+                      : totalContributions >= 10 && totalContributions < 50
+                      ? styles.totalContribDiv10
+                      : styles.totalContribDiv
+                  }
+                >
+                  <h2>
+                    Total Contributions Made:{" "}
+                    <CountUp
+                      start={1000000}
+                      end={totalContributions}
+                      duration={4.5}
+                      separator=","
+                    />
+                  </h2>
+                </div>
+              </CardContent>
+            </Card>
           </div>
-          {!isLoading && (
-            <TimeLines align="alternate">
-              {timelineData.map((item, index) => (
-                <TimelineItem key={index}>
-                  <TimelineOppositeContent>
-                    <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
-                      <p>
-                        <b>Last Updated at: </b>
-                        {moment(item.updated_at).format("MMM DD, YYYY")}
-                      </p>
-                    </ScrollAnimation>
-                  </TimelineOppositeContent>
-                  <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
-                  </TimelineSeparator>
-                  <TimelineContent key={index}>
-                    <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
-                      <a href={item.html_url} target="_blank">
-                        <Card className={styles.cards} variant="outlined">
-                          <CardContent align="center">
-                            <h2> {item.name} </h2>
-                            {item.description && (
-                              <p>
-                                <b>Description: </b>
-                                {item.description}
-                              </p>
-                            )}
-                            {item.homepage && (
-                              <p>
-                                <b>HomePage: </b>
-                                <a href={item.homepage} target="_blank">
-                                  <u>{item.homepage}</u>
-                                </a>
-                              </p>
-                            )}
-                            {!!item.contributions && (
-                              <p>
-                                <b>Contributions Made: </b>
-                                {item.contributions || ""}
-                              </p>
-                            )}
-                            <div className={styles.iconContainer}>
-                              {item.language && (
-                                <div className={styles.indivContainer}>
-                                  <div>
-                                    <CodeIcon />
-                                  </div>
-                                  <div className={styles.indivItems}>
-                                    {item.language}
-                                  </div>
-                                </div>
-                              )}
+          <TimeLines align="alternate">
+            {timelineData.map((item, index) => (
+              <TimelineItem key={index}>
+                <TimelineOppositeContent>
+                  <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+                    <p>
+                      <b>Last Updated at: </b>
+                      {moment(item.updated_at).format("MMM DD, YYYY")}
+                    </p>
+                  </ScrollAnimation>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                  <TimelineDot />
+                  <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent key={index}>
+                  <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+                    <a href={item.html_url} target="_blank">
+                      <Card className={styles.cards} variant="outlined">
+                        <CardContent align="center">
+                          <h2> {item.name} </h2>
+                          {item.description && (
+                            <p>
+                              <b>Description: </b>
+                              {item.description}
+                            </p>
+                          )}
+                          {item.homepage && (
+                            <p>
+                              <b>HomePage: </b>
+                              <a href={item.homepage} target="_blank">
+                                <u>{item.homepage}</u>
+                              </a>
+                            </p>
+                          )}
+                          <p className={styles.contributions}>
+                            <b>Contributions Made: </b>
+                            {item.contributions}
+                          </p>
+                          <div className={styles.iconContainer}>
+                            {item.language && (
                               <div className={styles.indivContainer}>
                                 <div>
-                                  <VisibilityIcon />
+                                  <CodeIcon />
                                 </div>
                                 <div className={styles.indivItems}>
-                                  {item.watchers_count}
+                                  {item.language}
                                 </div>
                               </div>
-                              <div className={styles.indivContainer}>
-                                <div>
-                                  <StarIcon />
-                                </div>
-                                <div className={styles.indivItems}>
-                                  {item.stargazers_count}
-                                </div>
+                            )}
+                            <div className={styles.indivContainer}>
+                              <div>
+                                <VisibilityIcon />
                               </div>
-                              <div className={styles.indivContainer}>
-                                <div>
-                                  <DeviceHubIcon />
-                                </div>
-                                <div className={styles.indivItems}>
-                                  {item.forks}
-                                </div>
+                              <div className={styles.indivItems}>
+                                {item.watchers_count}
                               </div>
                             </div>
-                          </CardContent>
-                        </Card>
-                      </a>
-                    </ScrollAnimation>
-                  </TimelineContent>
-                </TimelineItem>
-              ))}
-            </TimeLines>
-          )}
+                            <div className={styles.indivContainer}>
+                              <div>
+                                <StarIcon />
+                              </div>
+                              <div className={styles.indivItems}>
+                                {item.stargazers_count}
+                              </div>
+                            </div>
+                            <div className={styles.indivContainer}>
+                              <div>
+                                <DeviceHubIcon />
+                              </div>
+                              <div className={styles.indivItems}>
+                                {item.forks}
+                              </div>
+                            </div>
+                          </div>
+                        </CardContent>
+                      </Card>
+                    </a>
+                  </ScrollAnimation>
+                </TimelineContent>
+              </TimelineItem>
+            ))}
+          </TimeLines>
         </Paper>
       </Grid>
     </Grid>
